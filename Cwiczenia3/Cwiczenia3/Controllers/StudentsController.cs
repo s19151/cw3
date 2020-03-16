019296cb7ca37cdd01c5fabe34bcab3041339345
s19151 <s19151@pjwstk.edu.pl>
2020-03-16 16:27:38 +0100

    Dodanie kontrolera StudentsController

---
+++ Cwiczenia3/Cwiczenia3/Controllers/StudentsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Cwiczenia3.Controllers
+{
+    [ApiController]
+    [Route("api/students")]
+    public class StudentsController : ControllerBase
+    {
+        public String GetStudent() {
+            return "Kowalski, Malewski, Andrzejewski";
+        }
+    }
+}
