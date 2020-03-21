727cb5f98538fcbf45e2829d3067c44fd448a4e6
s19151 <s19151@pjwstk.edu.pl>
2020-03-21 17:42:12 +0100

    Dodanie metody obsługującą orderBy, zmiana ścieżki launchu

--- Cwiczenia3/Cwiczenia3/Controllers/StudentsController.cs
+++ Cwiczenia3/Cwiczenia3/Controllers/StudentsController.cs
@@ -13,5 +13,20 @@ namespace Cwiczenia3.Controllers
         public String GetStudent() {
             return "Kowalski, Malewski, Andrzejewski";
         }
+
+        [HttpGet("{id}")]
+        public IActionResult GetStudent(int id) {
+            if (id == 1)
+                return Ok("Kowalski");
+            else if (id == 2)
+                return Ok("Malewski");
+
+            return NotFound("Nie znaleziono studenta");
+        }
+
+        [HttpGet]
+        public String GetStudent(String orderBy) {
+            return $"Kowalski, Malewski, Andrzejewski sortowanie={orderBy}";
+        }
     }
 }
